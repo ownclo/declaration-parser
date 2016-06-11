9eebb47239de3ec7f60e8578c2f3d4c77d7b4e58
Ownclo - Arcis <mrownclo@gmail.com>
2016-06-11 16:06:49 +0300

    added sample schema

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
-from xlrd import open_workbook, XL_CELL_TEXT
+from xlrd import open_workbook
 import os
+import json
 
 dirname = "samples"
 fileName = "svedeniya-o-dohodah-sotrudnikov-territorialnyih-organov-roskomnadzora-2015.xls"
+schemaName = "schema.json"
+
+schema = json.load(open(schemaName))
 
 fname = os.path.join(dirname, fileName)
 
@@ -11,10 +15,9 @@ book = open_workbook(fname)
 sheet = book.sheet_by_index(0)
 
 cell = sheet.cell(0, 0)
-print cell
-print cell.value
-print cell.ctype == XL_CELL_TEXT
 
-for i in range(sheet.nrows):
-    #    print sheet.cell_type(1,i),sheet.cell_value(1,i)
-    print sheet.cell_value(i, 0)
+# for i in range(sheet.nrows):
+#     #    print sheet.cell_type(1,i),sheet.cell_value(1,i)
+#     print sheet.cell_value(i, 0)
+
+print schema
