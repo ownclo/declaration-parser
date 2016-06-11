9e57962a4dc90d2aea8d0a40609d4f9d23fc428f
uw5fl <uw5fl.2@gmail.com>
2016-06-11 15:49:23 +0300

    Reads 1st column from *.xls.

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
+from xlrd import open_workbook, XL_CELL_TEXT
 
-if __name__ == "__main__":
-    print "Hello, world!"
+FileName = "samples\svedeniya-o-dohodah-sotrudnikov-territorialnyih-organov-roskomnadzora-2015.xls"
+
+book = open_workbook(FileName)
+
+sheet = book.sheet_by_index(0)
+
+cell = sheet.cell(0, 0)
+print cell
+print cell.value
+print cell.ctype == XL_CELL_TEXT
+
+for i in range(sheet.nrows):
+    #    print sheet.cell_type(1,i),sheet.cell_value(1,i)
+    print sheet.cell_value(i, 0)
