1a6a715faa9ffa02fa091dca578bd90867662439
uw5fl <uw5fl.2@gmail.com>
2016-06-11 17:30:02 +0300

    reworked parser to process a single column and search for its name.

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-from xlrd import open_workbook
-import os
 import json
+import os
+
+from xlrd import open_workbook
 
 dirname = "samples"
 fileName = "svedeniya-o-dohodah-sotrudnikov-territorialnyih-organov-roskomnadzora-2015.xls"
@@ -14,11 +15,6 @@ book = open_workbook(fname)
 
 sheet = book.sheet_by_index(0)
 
-a = []
-for i in xrange(0, 10):
-	a.append(i)
-
-print a
 
 cell = sheet.cell(0, 0)
 
@@ -26,4 +22,34 @@ cell = sheet.cell(0, 0)
 #     #    print sheet.cell_type(1,i),sheet.cell_value(1,i)
 #     print sheet.cell_value(i, 0)
 
-print schema
+columnName = schema['columns'][0]['name']
+#print columnName
+
+columnFound = False
+
+print sheet.ncols
+print sheet.nrows
+
+def findColumn(sheet, columnName):
+	for i in xrange(0, sheet.nrows):
+		for j in xrange(0, sheet.ncols):
+			cell = sheet.cell_value(i, j)
+			if columnName in cell:
+				return (True, i, j)
+	return (False, sheet.nrows, sheet.ncols)
+
+def readColumnData (sheet, rowIndex, columnIndex):
+	result = []
+
+	for i in xrange(rowIndex + 1, sheet.nrows):
+		cellValue = sheet.cell_value(i, columnIndex)
+		if cellValue != "":
+			result.append((i, cellValue))
+	return result
+
+
+(isFound, row, column) = findColumn(sheet, columnName)
+
+if isFound:
+	column = readColumnData(sheet, row, column)
+	print column
