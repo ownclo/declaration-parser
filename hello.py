2709e794f2b9cc31d400f85a4b751ad5cce65225
uw5fl <uw5fl.2@gmail.com>
2016-06-12 12:56:40 +0300

    added subroutine FillSchemeDataByScheme

--- hello.py
+++ hello.py
@@ -28,11 +28,13 @@ columnName = schema['columns'][0]['name']
 print sheet.ncols
 print sheet.nrows
 
-def findColumn(sheet, columnName):
+#returns TRUE and the coordinates of the cell that contains the specified text if the search succeeded.
+#if the cell was not found, then FALSE and the size of the entire table is returned.
+def findCellWithText(sheet, TextToFind):
 	for i in xrange(0, sheet.nrows):
 		for j in xrange(0, sheet.ncols):
 			cell = sheet.cell_value(i, j)
-			if columnName in cell:
+			if TextToFind in cell:
 				return (True, i, j)
 	return (False, sheet.nrows, sheet.ncols)
 
@@ -67,13 +69,58 @@ def MergeColumnDataOnSheet (startingRowIndex, parsedUnmergedColumnData, parsedMe
 	for i in xrange (startingRowIndex, length):
 		(columnIndex, rowIndex, cellValue) = parsedUnmergedColumnData[i]
 		if cellValue != "":
-			result.append(((columnIndex, rowIndex, 1, 1), cellValue))
+			result.append(((columnIndex, rowIndex, columnIndex + 1, rowIndex + 1), cellValue))
 
-	for i in xrange (startingRowIndex, length):
+	for i in xrange (len(parsedMergedCellsData)):
 		result.append(parsedMergedCellsData[i])
 
 	result = sorted (sorted (result, key=lambda tup: tup[0][0]), key=lambda tup: tup[0][1])
 	return result
+
+def FillSchemeDataByScheme(InputData, Scheme):
+	result = []
+	SchemeElement = []
+	SchemeLength = len(Scheme)
+	InputDataLength = len(InputData)
+
+	for i in xrange (SchemeLength):
+		result.append([])
+		SchemeElement = Scheme["columns"][i]
+		for j in xrange (InputDataLength):
+			CellValue = InputData[j][1]
+			if SchemeElement["name"] in CellValue:
+				result[i].append(InputData[j])
+			else:
+				for k in xrange(len(SchemeElement["aliases"])):
+					if SchemeElement["aliases"][k] in CellValue:
+						result[i].append(InputData[j])
+	if len(result) == SchemeElement:
+		return (True, result)
+	else:
+		return (False, [])
+
+#remerges the list in multiple logical columns according to the initial data;
+#builds a table containing logical column coordinates
+#def BuildLogicalColumnsFromMergedDataset (InputData, ParsedScheme):
+#	result = []
+#	InputLength = len (InputData)
+#	SchemeLength = len(ParsedScheme)
+#
+#	for i in xrange(SchemeLength):
+#		CellValue = ParsedScheme[i][4]
+#		result.append(())
+#		(IsFound, x, y) = findCellWithText(CellValue)
+#		if IsFound:
+#			j = 0
+#			Found = False
+#			while j < InputLength and not Found:
+#				if CellValue == InputData[j][4]:
+#					result[i].append()
+#					Found = True
+#					j += 1
+#
+#
+#	return result
 
 #chooses records from the merged records list that have the specified column index
 def SelectColumnFromMergedSheetData (MergedColumnDataOnSheet, columnIndex):
@@ -81,7 +128,7 @@ def SelectColumnFromMergedSheetData (MergedColumnDataOnSheet, columnIndex):
 
 
 
-(isFound, row, column) = findColumn(sheet, columnName)
+(isFound, row, column) = findCellWithText(sheet, columnName)
 
 if isFound:
 	columnData = readUnmergedColumnData(sheet, row, column)
@@ -89,7 +136,9 @@ if isFound:
 #print readUnmergedColumnData(sheet, row, column)
 #print parseMergedCells(sheet)
 
-r = SelectColumnFromMergedSheetData(MergeColumnDataOnSheet(row, readUnmergedColumnData(sheet, row, column), parseMergedCells(sheet)), 9)
+MergedCells = MergeColumnDataOnSheet(row, readUnmergedColumnData(sheet, row, column), parseMergedCells(sheet))
+
+r = FillSchemeDataByScheme(MergedCells, schema)
 
 for ((xLow, yLow, xHigh, yHigh), s) in r:
-	print yLow, xLow, xHigh, yHigh, s
+	print xLow, yLow, xHigh, yHigh, s
